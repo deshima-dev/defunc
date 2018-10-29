dfa8f27ab563e3b1ac6b5fe51e7934c57e8d6f4b
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2018-10-29 17:29:39 +0900

    :sparkles: Update utility functions

--- defunc/utils.py
+++ defunc/utils.py
@@ -1,4 +1,5 @@
-__all__ = ['am']
+__all__ = ['exec_am',
+           'read_atm']
 
 
 # standard library
@@ -15,60 +16,56 @@ logger = getLogger(__name__)
 # dependent packages
 import numpy as np
 import pandas as pd
-import decode as dc
 import defunc as fn
 import astropy.units as u
 
 
 # module constants
 DIR_DATA = Path(fn.__path__[0]) / 'data'
+DEFAULT_AMC = DIR_DATA / 'ALMA_annual_50.amc'
+DEFAULT_ATM = DIR_DATA / 'ALMA_atm_model.data'
 
 
-def am(amc, *params, timeout=None, encoding='utf-8'):
+def exec_am(*params, amc=None, timeout=None, encoding='utf-8'):
     """Execute am and return output as pandas.DataFrame.
 
     Args:
-        amc (str or path): Path of am configuration file.
-            If it is just a file name and does not exist
-            in the current directory, the function will
-            look it up in the module's data directory.
-        params (list of str or float): Parameters of the
-            am configuration file. See example for detail.
+        params (str or float): Parameters of am configuration file.
+        amc (str or path, optional): Path of am configuration file.
+            If not spacified, package included ALMA_annual_50.amc is used.
 
     Returns:
         df (pandas.DataFrame): DataFrame that stores result
             values of am calculation.
 
     Example:
-        >>> df = am('ALMA_annual_50.amc', 330, 'GHz', 380, 'GHz',
-        ...         0.01, 'GHz', 0.0, 'deg', 1.0)
-
-        >>> df = am('ALMA_annual_50.amc', '330GHz', '380GHz',
-        ...         '0.01GHz', '0.0deg', 1.0)
+        >>> df = exec_am(330, 'GHz', 380, 'GHz', 0.01, 'GHz', 0.0, 'deg', 1.0)
+        >>> df = exec_am('330GHz', '380GHz', '0.01GHz', '0.0deg', 1.0)
 
     """
     # path of am
     am = os.environ.get('AM_PATH', 'am')
 
     # path of am configuration file
-    amc = Path(amc).expanduser()
-    if not amc.exists():
-        amc = DIR_DATA / amc.name
-        if not amc.exists():
-            raise FileNotFoundError
+    if amc is None:
+        path = DEFAULT_AMC
+    else:
+        path = Path(amc).expanduser()
 
-    # parse parameters
-    amp = []
+    # parse am parameters
+    params_ = []
+
     for param in params:
         unit = u.Unit(param)
         if unit.physical_type == 'dimensionless':
-            amp.append(unit.scale)
+            params_.append(unit.scale)
         else:
-            amp.extend(unit.to_string().split())
+            params_.extend(unit.to_string().split())
 
     # execute am
-    args = [str(p) for p in (am, amc, *amp)]
+    args = [str(p) for p in (am, path, *params_)]
     logger.info(f'Executing am with: {args}')
+
     cp = run(args, timeout=timeout, stdout=PIPE, stderr=PIPE)
     stdout = cp.stdout.decode(encoding)
     stderr = cp.stderr.decode(encoding)
@@ -78,12 +75,50 @@ def am(amc, *params, timeout=None, encoding='utf-8'):
 
     for line in stderr.split('\n'):
         matched = pattern.search(line)
+
         if matched:
             items = matched.groups()[0].split()
-            names, units = items[0::2], items[1::2]
+            kinds, units = items[0::2], items[1::2]
+            names = [f'{k} ({u})' for k, u in zip(kinds, units)]
             break
     else:
         raise RuntimeError(stderr)
 
     # return result as pandas.Dataframe
-    return pd.read_csv(io.StringIO(stdout), ' ', names=names)
+    df = pd.read_csv(io.StringIO(stdout), sep='\s+', index_col=0)
+    df.columns = names[1:]
+    df.index.name = names[0]
+    df.name = path.name
+    return df
+
+
+def read_atm(data=None, kind='tx'):
+    """Read ALMA ATM model's data and return it as pandas.DataFrame.
+
+    Args:
+        data (str or path, optilnal): Path of ALMA ATM model's data.
+            If not spacified, package included data is used.
+        kind (str, optional): Kind of values to be stored in DataFrame.
+            Must be either 'tx' (transmittance) or 'tau' (opacity).
+
+    Returns:
+        df (pandas.DataFrame): DataFrame that stores data.
+
+    """
+    if data is None:
+        path = DEFAULT_ATM
+    else:
+        path = Path(data).expanduser()
+
+    df = pd.read_csv(path, sep='\s+', index_col=0, comment='#')
+    df.columns = df.columns.astype(float)
+    df.index.name = 'frequency (GHz)'
+    df.columns.name = 'PWV (mm)'
+    df.name = kind
+
+    if kind == 'tx':
+        return df
+    elif kind == 'tau':
+        return -np.log(df)
+    else:
+        raise ValueError("kind must be either 'tx' or 'tau'")
