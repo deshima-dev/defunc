2a42f1874e75ce87f5a9028897caddb16036027a
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2018-10-30 01:02:04 +0900

    :fire: Delete namespace cleaning

--- defunc/__init__.py
+++ defunc/__init__.py
@@ -1,18 +1,5 @@
 __version__ = '2018.10.26'
 
 
-def tidy_namespace(module):
-    for name in dir(module):
-        if name.startswith('_'):
-            continue
-
-        if name in module.__all__:
-            continue
-
-        delattr(module, name)
-
-
 from . import utils
 from .funcs import *
-tidy_namespace(utils)
-tidy_namespace(funcs)
