daa6c2549935b71b9bacf4334d5796f7894571cd
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2018-11-08 16:47:27 +0900

    :art: Move functions between modules

--- defunc/calibration.py
+++ defunc/calibration.py
@@ -1,1 +1,67 @@
-__all__ = []
+__all__ = ['indexby',
+           'reallocate_scanid']
+
+
+# standard library
+from logging import getLogger
+logger = getLogger(__name__)
+
+
+# dependent packages
+import numpy as np
+import xarray as xr
+import defunc as fn
+
+
+def reallocate_scanid(array, t_divide=None, t_unit='s'):
+    """Reallocate scan ID of De:code array according to scan type.
+
+    Note that this will rewrite scan ID of the array in place.
+
+    Args
+        array (xarray.DataArray): Input array to be processed.
+        t_divide (int, optional): Minimum time interval in second.
+            If spacified, the function will allocate different scan ID
+            to adjacent two samples with time interval greater than
+            `t_divide` even if they have the same scan type.
+        t_unit (str, optional): This determines the unit of `t_divide`.
+
+    Returns:
+        array (xarray.DataArray): Array whose scan ID is reallocated.
+
+    """
+    fn.assert_isdarray(array)
+    time = array.time
+    scantype = array.scantype
+
+    cond = np.hstack([False, scantype[1:] != scantype[:-1]])
+
+    if t_divide is not None:
+        t_delta = np.timedelta64(int(t_divide), t_unit)
+        cond |= np.hstack([False, np.abs(np.diff(time)) > t_delta])
+
+    array.scanid.values = np.cumsum(cond)
+    return array
+
+
+def indexby(array, *items, coord='scantype'):
+    """Return boolean index of array coordinate matched by items.
+
+    Args:
+        array (xarray.DataArray): Input array.
+        items (string): Item values of coordinate to be selected.
+        coord (string, optional): Name of coodinate to be used.
+            Default is 'scantype'.
+
+    Returns:
+        boolean (xarray.DataArray): Boolean array.
+
+    """
+    fn.assert_isdarray(array)
+    coord = array[coord]
+    index = xr.zeros_like(coord, bool)
+
+    for item in items:
+        index |= (coord==item)
+
+    return index
--- defunc/utils.py
+++ defunc/utils.py
@@ -8,8 +8,6 @@ __all__ = ['exec_am',
            'denormalize',
            'show_versions',
            'import_packages',
-           'reallocate_scanid',
-           'indexby',
            'show_funclist']
 
 
@@ -319,60 +317,6 @@ def import_packages(where='<module>'):
         f_globals[module] = import_module(fullname)
         logger.info(message)
         print(message)
-
-
-def reallocate_scanid(array, t_divide=None, t_unit='s'):
-    """Reallocate scan ID of De:code array according to scan type.
-
-    Note that this will rewrite scan ID of the array in place.
-
-    Args
-        array (xarray.DataArray): Input array to be processed.
-        t_divide (int, optional): Minimum time interval in second.
-            If spacified, the function will allocate different scan ID
-            to adjacent two samples with time interval greater than
-            `t_divide` even if they have the same scan type.
-        t_unit (str, optional): This determines the unit of `t_divide`.
-
-    Returns:
-        array (xarray.DataArray): Array whose scan ID is reallocated.
-
-    """
-    fn.assert_isdarray(array)
-    time = array.time
-    scantype = array.scantype
-
-    cond = np.hstack([False, scantype[1:] != scantype[:-1]])
-
-    if t_divide is not None:
-        t_delta = np.timedelta64(int(t_divide), t_unit)
-        cond |= np.hstack([False, np.abs(np.diff(time)) > t_delta])
-
-    array.scanid.values = np.cumsum(cond)
-    return array
-
-
-def indexby(array, *items, coord='scantype'):
-    """Return boolean index of array coordinate matched by items.
-
-    Args:
-        array (xarray.DataArray): Input array.
-        items (string): Item values of coordinate to be selected.
-        coord (string, optional): Name of coodinate to be used.
-            Default is 'scantype'.
-
-    Returns:
-        boolean (xarray.DataArray): Boolean array.
-
-    """
-    fn.assert_isdarray(array)
-    coord = array[coord]
-    index = xr.zeros_like(coord, bool)
-
-    for item in items:
-        index |= (coord==item)
-
-    return index
 
 
 def show_funclist(kind='list'):
