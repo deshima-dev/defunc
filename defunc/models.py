80f985d39eaef91f475aa5c1fb28c08b000a3d5a
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-07-08 17:58:37 +0900

    :sparkles: Add SRUSP LASSO function

--- defunc/models.py
+++ defunc/models.py
@@ -1,5 +1,6 @@
 __all__ = ['calibrate_arrays',
            'estimate_baseline',
+           'estimate_baseline_lasso',
            'estimate_commonmode',
            'make_continuummap']
 
@@ -18,6 +19,7 @@ from scipy.interpolate import interp1d
 from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
 from sklearn.decomposition import TruncatedSVD
+from tqdm import tqdm
 
 
 # module constants
@@ -132,6 +134,72 @@ def estimate_baseline(Ton, Tamb=273.0, order=0, weight=None,
         Tbase.coords[f'coeff_{i}'] = 't', model.coef_[:, i]
 
     return Tbase
+
+
+def estimate_baseline_lasso(Ton, Tamb=273.0, order=0, timechunk=80,
+                            cv=None, progress=True, **kwargs):
+    """Estimate ultra-wideband baseline using the multi-task LASSO.
+
+    Args:
+        Ton (xarray.DataArray): Calibrated De:code array of ON point.
+        Tamb (float, optional): Ambient temperature used in calibration.
+        order (int, optional): Maximum order of a polynomial function
+            which is assumed to represent a continuum emission spectrum.
+            Default is 0 (flat continuum emission).
+        timechunk (int, optional): The number of samples to be used for
+            a multi-task LASSO. Default is 80 (~0.5 s for DESHIMA data).
+        cv (int, optional): The number of fold for cross validation (CV).
+            If not spacified, CV is not conducted (default alpha is used).
+        progress (bool, optional): If True, then a progress bar is shown.
+        kwargs (dict, optional): Keyword arguments for model initialization.
+
+    Returns:
+        Tbase (xarray.DataArray): De:code array of estimated baseline.
+
+    """
+    freq = np.asarray(Ton.kidfq).copy()
+    slope = fn.models._calculate_dtau_dpwv(freq)
+    freq -= np.median(freq)
+
+    N_freq = len(freq)
+    N_poly = order + 1
+
+    X = np.zeros([N_freq, N_poly+1])
+    X[:, 0] = slope / np.linalg.norm(slope)
+
+    for i in range(N_poly):
+        poly = freq**i
+        X[:, i+1] = poly / np.linalg.norm(poly)
+
+    default_kwargs = {'fit_intercept': False}
+    kwargs = {**default_kwargs, **kwargs}
+    n_chunk = int(len(Ton) / timechunk)
+    is_cv = cv is not None and cv>1
+
+    if is_cv:
+        model = linear_model.MultiTaskLassoCV(cv=cv, **kwargs)
+    else:
+        model = linear_model.MultiTaskLasso(**kwargs)
+
+    with tqdm(total=n_chunk, disable=not progress) as bar:
+        def func(Ton_):
+            model.fit(X, Ton_.values.T)
+
+            Tbase_ = np.outer(model.coef_[:, 0], X[:, 0])
+            Tbase_ = dc.full_like(Ton_, Tbase_)
+
+            for i in range(N_poly+1):
+                Tbase_.coords[f'basis_{i}'] = 'ch', X[:, i]
+                Tbase_.coords[f'coeff_{i}'] = 't', model.coef_[:, i]
+
+            if is_cv:
+                alpha = np.full(len(Ton_), model.alpha_)
+                Tbase_.coords['alpha'] = 't', alpha
+
+            bar.update(1)
+            return Tbase_
+
+        return Ton.groupby_bins('t', n_chunk).apply(func)
 
 
 def _calculate_dtau_dpwv(freq):
